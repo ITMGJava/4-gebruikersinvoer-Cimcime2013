26467ca7285d972664d1a4003556159a3f94f945
cimcim <sergensol@gmail.com>
2020-09-24 13:42:15 +0200

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	out/production/4-gebruikersinvoer-Cimcime2013/GebruikersInvoer.class
    #	src/GebruikersInvoer.java

---
+++ src/Opgave4.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Opgave4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Voer een woord in");
+        Scanner invoer = new Scanner(System.in);
+        String woord = invoer.nextLine();
+        System.out.println(woord.length());
+        System.out.println("Voer een getal in");
+        int getal = invoer.nextInt();
+        System.out.println("de tafel van 288 is: ");
+        int uitkomst = getal * 1;
+        int uitkomst2 = getal * 2;
+        System.out.println("1 * " + getal + " = " + uitkomst);
+        System.out.println("1 * " + getal + " = " + uitkomst2);
+
+
+
+    }
+}
