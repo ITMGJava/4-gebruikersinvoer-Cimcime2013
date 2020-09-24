805b39d09b56f61d3c08770515025817298242af
cimcim <sergensol@gmail.com>
2020-09-24 13:23:18 +0200

    hierbij het antwoord

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -9,6 +9,16 @@ public class GebruikersInvoer {
     String woord = invoer.nextLine();
         System.out.println(woord.length());
 
+        System.out.println("Voer een getal in");
+         int getal  = invoer.nextInt();
+        System.out.println("de tafel van 288 is: ");
+        int uitkomst = getal*1;
+        int uitkomst2 = getal*2;
+        System.out.println(1 + " * " + getal+ " = " + uitkomst);
+        System.out.println(1 + " * " + getal+ " = " + uitkomst2);
+
+
+
 
 
 
