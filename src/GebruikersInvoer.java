5a00a3cf83d5d4952f43d17618ac9cde58c95582
cimcim <sergensol@gmail.com>
2020-09-24 12:38:47 +0200

    hierbij het antwoord

--- src/GebruikersInvoer.java
+++ src/GebruikersInvoer.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class GebruikersInvoer {
+    public static void main(String[] args) {
+
+        System.out.println("Voer een woord in");
+
+        Scanner invoer = new Scanner(System.in);
+    String woord = invoer.nextLine();
+        System.out.println(woord.length());
+
+
+
+
+
+    }
+
 }
