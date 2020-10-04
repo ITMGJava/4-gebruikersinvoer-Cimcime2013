921ee985761c96143760e18a23e8395f98095ba1
cimcim <sergensol@gmail.com>
2020-10-04 12:10:43 +0200

    opgave 4kant

---
+++ src/Vierkant.java
@@ -0,0 +1,73 @@
+public class Vierkant {
+
+
+    public static void main(String[] args) {
+
+
+        for(int b =0;b<10;b++){
+
+            System.out.print("0"+b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =10;b<20;b++){
+            System.out.print(b+ "  ");
+        }
+
+        {
+            System.out.println();
+        }
+
+        for(int b =20;b<30;b++){
+            System.out.print(b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =30;b<40;b++){
+            System.out.print(b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =40;b<50;b++){
+            System.out.print(b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =50;b<60;b++){
+            System.out.print(b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =60;b<70;b++){
+            System.out.print(b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =70;b<80;b++){
+            System.out.print(b+ "  ");
+        }
+        {
+            System.out.println();
+        }
+
+        for(int b =80;b<90;b++){
+            System.out.print(b+ "  ");
+        }
+
+
+
+    }
+
+}
